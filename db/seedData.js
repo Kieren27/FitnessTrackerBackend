ebe53da909fcce1c5082c1a6e86ab6f28b0a50f0
Kieren Enright <kierenenright27@gmail.com>
2023-05-31 21:07:51 -0700

    updated createTables() queries

--- db/seedData.js
+++ db/seedData.js
@@ -25,14 +25,15 @@ async function createTables() {
   CREATE TABLE activities (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) UNIQUE NOT NULL,
-    description VARCHAR(255) NOT NULL
+    description TEXT NOT NULL
   );
 
   CREATE TABLE routines (
+    id SERIAL PRIMARY KEY,
     "creatorId" INTEGER REFERENCES users(id) NOT NULL,
     name VARCHAR(255) UNIQUE NOT NULL,
-    goal VARCHAR(255) NOT NULL,
-    "isPublic" BOOLEAN DEFAULT true
+    goal TEXT NOT NULL,
+    "isPublic" BOOLEAN DEFAULT false
   );
 `)
 }
