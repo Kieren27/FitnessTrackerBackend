dafac905e45261d46b5a82205302cfcdc4c9bd5f
Kieren Enright <kierenenright27@gmail.com>
2023-06-01 21:01:45 -0700

    updated all functions with try/catch wrappers

--- db/users.js
+++ db/users.js
@@ -7,8 +7,9 @@ async function createUser({ username, password }) {
 
   const SALT_COUNT = 10;
   const hashedPassword = await bcrypt.hash(password, SALT_COUNT)
-  
-    const {rows} = await client.query(`
+
+  try {
+    const { rows } = await client.query(`
     INSERT INTO users(username, password)
     VALUES ($1, $2)
     ON CONFLICT (username) DO NOTHING
@@ -16,45 +17,58 @@ async function createUser({ username, password }) {
     `, [username, hashedPassword])
 
     return rows;
-  
-  
+  } catch (error) {
+    throw (error);
+  }
 }
 
 async function getUser({ username, password }) {
-  const user = await getUserByUsername(username);
-  const hashedPassword = user.password;
-  const isValid = await bcrypt.compare(password, hashedPassword);
+  try {
+    const user = await getUserByUsername(username);
+    const hashedPassword = user.password;
+    const isValid = await bcrypt.compare(password, hashedPassword);
 
-  if(isValid){
-    return user;
+    if (isValid) {
+      return user;
+    }
+  } catch (error) {
+    throw (error);
   }
 }
 
 async function getUserById(userId) {
-  
-    const {rows } = await client.query(`
-    SELECT * FROM users
-    WHERE id=$1;
-    `, [userId])
 
-    if (!rows.length){
+  try {
+    const { rows } = await client.query(`
+  SELECT * FROM users
+  WHERE id=$1;
+  `, [userId])
+
+    if (!rows.length) {
       return null
     } else {
       delete rows[0].password;
       return rows[0];
     }
-  
+  } catch (error) {
+    throw (error);
+  }
+
 }
 
 async function getUserByUsername(userName) {
-const {rows: [user]} = await client.query(`
+  try {
+    const { rows: [user] } = await client.query(`
   SELECT *
   FROM users
   WHERE username = $1;
   
   `, [userName]);
 
-  return user;
+    return user;
+  } catch (error) {
+    throw (error);
+  }
 }
 
 module.exports = {
