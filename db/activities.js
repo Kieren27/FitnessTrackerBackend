4bbf89229b38ca15389f83bdc8b073c521d12be0
Shruthi Tuplur <shruthituplur@gmail.com>
2023-06-01 20:49:35 -0700

    created createActivity function which adds a new activity into the database and also getAllActivities function which returns all activities currently in the database

--- db/activities.js
+++ db/activities.js
@@ -2,11 +2,25 @@ const client = require('./client');
 
 // database functions
 async function createActivity({ name, description }) {
+  
+    const {rows: [activity]} = await client.query(`
+    INSERT INTO activities(name, description)
+    VALUES ($1, $2)
+    RETURNING *;
+    
+    `, [name, description])
+  
   // return the new activity
+  return activity;
 }
 
 async function getAllActivities() {
   // select and return an array of all activities
+  const {rows} = await client.query(
+    `SELECT *
+    FROM activities;`
+  )
+  return rows;
 }
 
 async function getActivityById(id) {}
