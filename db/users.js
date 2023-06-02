1528cfe0242176a4f0d904b5fcf6b6e8dbe375b0
Shruthi Tuplur <shruthituplur@gmail.com>
2023-06-01 20:30:13 -0700

    created getUserById function

--- db/users.js
+++ db/users.js
@@ -1,18 +1,51 @@
 const client = require("./client");
-
+const bcrypt = require('bcrypt');
 // database functions
 
 // user functions
 async function createUser({ username, password }) {
+
+  const SALT_COUNT = 10;
+  const hashedPassword = await bcrypt.hash(password, SALT_COUNT)
+  try{
+    const {rows} = await client.query(`
+    INSERT INTO users(username, password)
+    VALUES ($1, $2)
+    ON CONFLICT (username) DO NOTHING
+    RETURNING *;
+    `, [username, hashedPassword])
+  } catch (error) {
+    throw error;
+  }
   
 }
 
 async function getUser({ username, password }) {
+  const user = await getUserByUsername(username);
+  const hashedPassword = user.password;
+  const isValid = await bcrypt.compare(password, hashedPassword);
 
+  if(isValid){
+    return user;
+  }
 }
 
 async function getUserById(userId) {
+  try {
+    const {rows } = await client.query(`
+    SELECT * FROM users
+    WHERE id=$1;
+    `, [userId])
 
+    if (!rows.length){
+      return null
+    } else {
+      delete rows[0].password;
+      return rows[0];
+    }
+  } catch (error){
+    throw error;
+  }
 }
 
 async function getUserByUsername(userName) {
