2a432c4884dd692eddbafa08c3c0c295cc0fa527
code3bery <nerady2007@gmail.com>
2023-06-08 20:44:18 -0700

    added AddActivitiesToRoutine

--- db/routine_activities.js
+++ db/routine_activities.js
@@ -1,11 +1,18 @@
 const client = require("./client");
 
-async function addActivityToRoutine({
-  routineId,
-  activityId,
-  count,
-  duration,
-}) {}
+async function addActivityToRoutine({ routineId, activityId, count, duration }) {
+  try {
+    const { rows: [routineActivity] } = await client.query(`
+      INSERT INTO routine_activities("routineId", "activityId", count, duration)
+      VALUES ($1, $2, $3, $4)
+      RETURNING *;
+    `, [routineId, activityId, count, duration]);
+
+    return routineActivity;
+  } catch (error) {
+    throw error;
+  }
+}
 
 async function getRoutineActivityById(id) {}
 
