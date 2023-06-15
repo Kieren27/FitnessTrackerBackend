f253e260b4f18046e33f5e9e2e04db3459f40f9c
Kieren Enright <kierenenright27@gmail.com>
2023-06-15 16:19:38 -0700

    Add JWT verification

--- api/index.js
+++ api/index.js
@@ -1,9 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const { getUserById } = require('../db');
+const { JWT_SECRET } = process.env;
 
 // GET /api/health
 router.get('/health', async (req, res, next) => {
     res.send({message: "Server is funcitoning properly"});
+});
+
+router.use(async (req, _res, next) => {
+    const prefix = 'Bearer ';
+    const auth = req.header('Authorization');
+
+    if (!auth) {
+        next();
+    } else if (auth.startsWith(prefix)) {
+        const token = auth.slice(prefix.length);
+
+        try {
+            const { id } = jwt.verify(token, JWT_SECRET);
+
+            if (id) {
+                req.user = await getUserById(id);
+                next();
+            }
+        } catch (error) {
+            throw error;
+        }
+    } else {
+        next({
+            name: 'AuthorizationHeaderError',
+            message: `Authorization token must start with ${prefix}`
+        });
+    }
 });
 
 // ROUTER: /api/users
