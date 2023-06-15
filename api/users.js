5c1eaaeba85c2a96941d60f347ff3303e9b776ca
Kieren Enright <kierenenright27@gmail.com>
2023-06-15 16:50:31 -0700

    users.js: route usersRouter.post('/register') finished + passing all tests

--- api/users.js
+++ api/users.js
@@ -1,14 +1,60 @@
 /* eslint-disable no-useless-catch */
 const express = require("express");
-const router = express.Router();
+const usersRouter = express.Router();
+
+const jwt = require('jsonwebtoken');
+const { getUserByUsername, createUser } = require("../db");
 
 // POST /api/users/register
+usersRouter.post('/register', async (req, res, next) => {
+    const { username, password } = req.body;
 
+    try {
+        const _user = await getUserByUsername(username);
+
+        if (_user) {
+            res.send({
+                message: `User ${_user.username} is already taken.`,
+                name: 'UserExistsError',
+                error: 'Error creating new user as that username already exists'
+            });
+        }
+
+        if (password.length < 8) {
+            res.send({
+                error: 'Error Creating Password: Password must be at least 8 or more characters',
+                message: "Password Too Short!",
+                name: "PasswordLengthError"
+            });
+        }
+
+        const user = await createUser({
+            username,
+            password
+        });
+
+        const token = jwt.sign({
+            id: user.id,
+            username
+        }, process.env.JWT_SECRET, { expiresIn: '1w' });
+
+        res.send({
+            user: {
+                id: user.id,
+                username: user.username
+            },
+            message: "thank you for signing up",
+            token
+        });
+    } catch ({ name, message }) {
+        next({ name, message });
+    }
+});
 // POST /api/users/login
 
 // GET /api/users/me
 
 // GET /api/users/:username/routines
 
-module.exports = router;
 module.exports = usersRouter;
+
