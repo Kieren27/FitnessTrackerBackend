cf56e8875207b78b6bd91744e419f8c325f1492a
Kieren Enright <kierenenright27@gmail.com>
2023-06-19 16:34:12 -0700

    api/routineActivities.js: added (PATCH /:routineActivityId) that allows logged in user to update routineActivity if they created the routine

--- api/routineActivities.js
+++ api/routineActivities.js
@@ -1,8 +1,42 @@
 const express = require('express');
-const router = express.Router();
+const routineActivitiesRouter = express.Router();
+
+const { 
+    updateRoutineActivity,
+    canEditRoutineActivity,
+    getRoutineActivityById,
+    getRoutineById,
+} = require('../db');
+const { requireUser } = require("./utils");
 
 // PATCH /api/routine_activities/:routineActivityId
+routineActivitiesRouter.patch('/:routineActivityId', requireUser, async (req, res, next) => {
+    const { routineActivityId } = req.params;
+    const { count, duration} = req.body;
 
+    console.log("routineActivityId: ", routineActivityId);
+
+    try {
+       const canEdit = await canEditRoutineActivity(routineActivityId, req.user.id);
+
+       const activityToUpdate = await getRoutineActivityById(routineActivityId);
+       const { id, routineId } = activityToUpdate;
+       const routine = await getRoutineById(routineId);
+
+       if (canEdit) {
+        const updatedRoutineActivity = await updateRoutineActivity({id, count, duration});
+        res.send(updatedRoutineActivity);
+       } else {
+        res.status(403).json({
+            error: "Unauthorized User",
+            name: "UnauthorizedUserError",
+            message: `User ${req.user.username} is not allowed to update ${routine.name}`
+        });
+       }
+    } catch ({ name, message }) {
+        next({ name, message });
+    }
+});
 // DELETE /api/routine_activities/:routineActivityId
 
-module.exports = router;
+module.exports = routineActivitiesRouter;
