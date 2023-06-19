fc472ff890af333c6ff359d85d1b8e0a8f3dab2d
Kieren Enright <kierenenright27@gmail.com>
2023-06-19 11:01:30 -0700

    routines.js: add (POST /api/routines) that creates a new routine, requires a user to be logged in

--- api/routines.js
+++ api/routines.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const routinesRouter = express.Router();
 
-const { getAllPublicRoutines } = require('../db');
+const { 
+    getAllPublicRoutines,
+    createRoutine,
+ } = require('../db');
+const { requireUser } = require('./utils');
 
 // GET /api/routines
 routinesRouter.get('/', async (req, res, next) => {
@@ -14,7 +18,23 @@ routinesRouter.get('/', async (req, res, next) => {
     }
 });
 // POST /api/routines
+routinesRouter.post('/', requireUser, async (req, res, next) => {
+    const { isPublic, name, goal } = req.body;
 
+    const routineData = {};
+    try {
+        routineData.creatorId = req.user.id;
+        routineData.name = name;
+        routineData.goal = goal;
+        routineData.isPublic = isPublic;
+
+        const newRoutine = await createRoutine(routineData);
+
+        res.send(newRoutine);
+    } catch ({ name, message }) {
+        next({ name, message });
+    }
+});
 // PATCH /api/routines/:routineId
 
 // DELETE /api/routines/:routineId
